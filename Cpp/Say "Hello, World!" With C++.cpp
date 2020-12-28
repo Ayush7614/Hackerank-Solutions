5a35e999160804d800517782cad05e648aa6cf30
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2020-12-28 09:27:15 +0530

    Create Say "Hello, World!" With C++.cpp

---
+++ Cpp/Say "Hello, World!" With C++.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+    cout<<"Hello, World!";
+    //printf("Hello, World!");
+    return 0;
+}
