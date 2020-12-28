49d1c52257546e9f4f793c023ccbd6ba0f28417a
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2020-12-28 09:28:06 +0530

    Create Input and Output.cpp

---
+++ Cpp/Input and Output.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    
+   long double a,b,c,sum=0;
+
+    cin>>a>>b>>c;
+
+    sum=a+b+c;
+
+    cout<<sum;
+
+  
+    return 0;
+}
