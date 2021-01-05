da6cda1fd5ddab80c56a2b5b0c7641044ac2c790
Sumitbatraa <76881949+Sumitbatraa@users.noreply.github.com>
2021-01-05 15:57:22 +0530

    Update Arithmetic Operators.py
    I have added some more operators ,
    and comments with each operators just to make it understand easily ,
    Hope you will merge these changes in main code,
    Thanku..

--- Python/Arithmetic Operators.py
+++ Python/Arithmetic Operators.py
@@ -1,6 +1,9 @@
 a = int(raw_input())
 b = int(raw_input())
 
-print a + b
-print a - b
-print a * b
+print (a + b)### ADDS THE TWO INTEGERS ##
+print (a - b)### SUBTRACT THE TWO INTEGERS ##
+print (a * b)### MULTIPLY 'a' WITH 'b' ##
+print (a / b)### DIVIDES a AND b ##
+print (a % b)### GIVES THE REMAINDER AFTER DIVISION OF TWO INTEGER##
+print (a ** b)### PERFORMS EXPONENTIAL (POWER) LIKE A**B =10 TO THE POWER 20 ##
